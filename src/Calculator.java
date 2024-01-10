17fff78b23ce7d0267cdd1943c96d86bc8e3ec4d
NovaS1de <nova29e@gmail.com>
2024-01-10 20:42:51 +0400

    Second commit

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public double slozhit(double a, double b){
+        return a+b;
+    }
+
+    public double vichest(double a, double b){
+        return a-b;
+    }
+
+    public double umnozhit(double a, double b){
+        return a*b;
+    }
+
+    public double podelit(double a, double b){
+        return a/b;
+    }
+
+}
